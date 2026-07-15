a9af9b456c97ee00d84c7f99d8b63e516b542c28
Urvi1408 <winsome.urvi@gmail.com>
2026-07-15 16:41:21 +0530

    improved implementation

--- LeetCode/C++/Arrays/Merge_Sorted_Array.cpp
+++ LeetCode/C++/Arrays/Merge_Sorted_Array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int k=0;
-        for(int i=m;i<nums1.size();i++)
+        for(int i=m;i<m+n;i++)
         {
             nums1[i]=nums2[k];
             k++;
