6589d076ffa1864bb75d9c924243fcaf71c52763
Urvi1408 <winsome.urvi@gmail.com>
2026-07-16 13:23:39 +0530

    optimised solution

--- LeetCode/C++/Arrays/Best_time_to_buy_and_sell_stocks.cpp
+++ LeetCode/C++/Arrays/Best_time_to_buy_and_sell_stocks.cpp
@@ -1,19 +1,40 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int profit=0;
+//         int p=0;
+//         for(int i=0;i<prices.size();i++)
+//         {
+//             for (int j=i+1;j<prices.size();j++)
+//             {
+//                 p=prices[j]-prices[i];
+//                 if (p>profit)
+//                 {
+//                     profit=p;
+//                 }
+//             }
+//         }
+//         return profit;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        int p=0;
-        for(int i=0;i<prices.size();i++)
+        int maxprofit=0;
+        int minprice=prices[0];
+        for (int j=0;j<prices.size();j++)
         {
-            for (int j=i+1;j<prices.size();j++)
+            if (prices[j]<minprice)
             {
-                p=prices[j]-prices[i];
-                if (p>profit)
-                {
-                    profit=p;
-                }
+                minprice=prices[j];
             }
+            int profit= prices[j]-minprice;
+            if (profit>maxprofit)
+            {
+                maxprofit=profit;
+            }
+             
         }
-        return profit;
+        return maxprofit;
     }
-};
