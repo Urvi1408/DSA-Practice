e5a5241db460577d4ed5b33448363af1007c9a17
Urvi1408 <winsome.urvi@gmail.com>
2026-07-16 16:37:25 +0530

    did pascal triangle problem

---
+++ LeetCode/C++/Arrays/pascals_triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> answer;
+        for (int i=0;i<numRows;i++)
+        {
+            vector<int>newrow;
+            newrow.push_back(1);
+            for(int j=0;j<i-1;j++)
+            {
+                newrow.push_back(answer[i-1][j]+answer[i-1][j+1]);
+            }
+            if (i>0)
+            {
+                newrow.push_back(1);
+            }
+            answer.push_back(newrow);
+        }
+        return answer;
+    }
+};
